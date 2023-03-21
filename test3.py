3e23f8ec242060f6cbe01acb06e3261a380a2fe9
Yuri <micabris@yandex.ru>
2023-03-21 23:57:36 +0300

    new file

---
+++ test3.py
@@ -0,0 +1,7 @@
+counter = int(input("Количество чисел:"))
+y = 0
+for i in range(counter):
+    x = float(input("{} Число: ".format(i + 1)))
+    y += x
+y = y / counter
+print("Среднее арифметическое: " + str(y))
